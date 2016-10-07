145f297eca0f27452e65ccd3b25653ca3555be46
Jan <jan.ficko@gmail.com>
2016-10-07 18:52:45 +0200

    Created database migration, model and controller for chat.

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Chat::orderBy('created_at', 'desc')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
