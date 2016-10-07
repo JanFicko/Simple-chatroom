8e88ff1a1d493c1b505b1481b840384597f5afd6
Jan <jan.ficko@gmail.com>
2016-10-07 19:00:14 +0200

    Added bootstrap, jquery, bootstrap libraries. Defined some files in view. Refactored welcome file name to index.

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html ng-app="chatApp">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Simple chatroom</title>
+
+    <!-- Styles -->
+    <link href="<% asset('css/bootstrap.min.css') %>" rel="stylesheet"/>
+    @yield('style')
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+<!-- Scripts -->
+<script src="<% asset('js/libs/angular.min.js') %>"></script>
+<script src="<% asset('js/libs/angular-route.min.js') %>"></script>
+<script src="<% asset('js/libs/jquery.min.js') %>"></script>
+<script src="<% asset('js/libs/bootstrap.min.js') %>"></script>
+
+@yield('script')
+
+</body>
+
+</html>
